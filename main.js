15fffedfa147c9e746a3acaeb5b4fc0f92f853b3
Daniel Camucatto <daniel.camucatto@gmail.com>
2022-09-27 21:34:00 -0300

    add create

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "teste1", 
+    email: "teste@testa.com", 
+    celular: "11123459876", 
+    cidade: "São Paulo"
+}    
+
+//CRUD
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (db_client) => localStorage.setItem("db_client", JSON.stringify(db_client));
+//func CREATE
+
+const createClient = (client) => {
+    const db_client = getLocalStorage();
+    db_client.push(client);
+    setLocalStorage(db_client)
+    
+}
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
